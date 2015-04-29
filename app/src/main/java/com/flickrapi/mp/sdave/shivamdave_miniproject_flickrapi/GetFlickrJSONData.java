87cc0063514ef0fbf0ee98e4d6b921be72f43e5e
Shivam Dave <sdave@ucsc.edu>
2015-04-29 09:00:50 -0700

    Finishes up the GetFlickrJSONData class by creating the parsed JSON list of images called during the post execute stage

--- app/src/main/java/com/flickrapi/mp/sdave/shivamdave_miniproject_flickrapi/GetFlickrJSONData.java
+++ app/src/main/java/com/flickrapi/mp/sdave/shivamdave_miniproject_flickrapi/GetFlickrJSONData.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.net.Uri;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +29,9 @@ public class GetFlickrJSONData extends GetData {
 
     public void execute() {
         super.setDataURL(endpointURI.toString());
-        // TODO: Does the actual execution
+        DownloadJSONData jsonDataDownloader = new DownloadJSONData();
+        Log.e(LOG_TAG, "Endpoint URI: " + endpointURI.toString());
+        jsonDataDownloader.execute(endpointURI.toString());
     }
 
     // Makes the FLICKR_URL string a uri
@@ -42,16 +48,54 @@ public class GetFlickrJSONData extends GetData {
         }
     }
 
+    // Assembles the list of pictures that hold the title, link, and image
+    // of each pic. Also logs the list afterwards. This List of images
+    // will be used to be fed into our adapter
     public void createResult() {
-        // TODO: Does the actual parsing of the JSON into a list of images
+        if (getDownloaderState() != DownloaderState.WORKING) {
+            Log.e(LOG_TAG, "DownloaderState = NOT WORKING");
+        }
+
+        final String flickrItems = "items";
+        final String flickrTitle = "title";
+        final String flickrLink = "link";
+        final String flickrImageUrl = "m";
+        final String flickrMedia = "media";
+
+        try {
+            JSONObject jsonData = new JSONObject(getDataRaw());
+            JSONArray itemsArr = jsonData.getJSONArray(flickrItems);
+            for (int i = 0; i < itemsArr.length(); i++) {
+                JSONObject jsonImage = itemsArr.getJSONObject(i);
+                String jsonTitle = jsonImage.getString(flickrTitle);
+                String jsonLink = jsonImage.getString(flickrLink);
+
+                JSONObject jsonMedia = jsonImage.getJSONObject(flickrMedia);
+                String imageUrl = jsonMedia.getString(flickrImageUrl);
+
+                Image makeImg = new Image(jsonTitle, jsonLink, imageUrl);
+                this._images.add(makeImg);
+            }
+            int i = 0;
+            for (Image oneImg : _images) {
+                i++;
+                Log.v(LOG_TAG, "Image" + "(" + Integer.toString(i) + "): " + oneImg.toString());
+            }
+        } catch (JSONException err) {
+            err.printStackTrace();
+            Log.e(LOG_TAG, "JSON EXECPTION ERROR: " + err.toString());
+        }
     }
 
+    // Returns the imageList later, when we have
+    // to pass it to the adapter
     public List<Image> getImages() {
         return _images;
     }
 
     public class DownloadJSONData extends GetJSONData {
 
+        // After executing the _image list is created
         protected void onPostExecute(String webData) {
             super.onPostExecute(webData);
             createResult();
